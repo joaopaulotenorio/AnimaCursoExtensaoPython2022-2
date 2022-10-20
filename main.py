6d5afdca45b5263fefe55edeb87e8465651fd975
João Paulo Tenório <44954773+joaopaulotenorio@users.noreply.github.com>
2022-10-20 01:13:18 +0000

    Exemplos de formatações de valores para string no momento da impressao

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
 # Meu primeiro projeto Python!!!
 #
 # print() = comando de saida
-print("Ola mundo!")
+print("Ola mundo!\n")
 
 # Quando quiser guardar uma String! (frase)
 nome = "Joao Paulo Tenorio"
+
 # Quando quiser guardar um número inteiro 
 idade = 24
+
 # Exibir o meu nome (que está dentro da variável nome)
-print(nome)
+print(nome+"\n")
+
+# Quando quiser exibir a frase "Minha idade é " completando com o conteúdo da variável idade
+# print("Minha idade é "+nome)
+# print("Minha idade é ", idade)
+# str converte numero para string no momento da impressao
+print("Minha idade é "+str(idade))
+# f""{} conversao de numero para string no momento da impressao
+print(f"\nMinha idade é {idade}\n")
+# " {}".format(numero) converte para string no momento da impressao
+print("Minha idade é {}\n".format(idade))
+
 
 ''' 
 Comentario em bloco
