c2773b606979655d0e7e3b54b8fe79ff69cb559d
João Paulo Tenório <44954773+joaopaulotenorio@users.noreply.github.com>
2022-10-20 00:50:04 +0000

    Primeiros Comandos

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 # Meu primeiro projeto Python!!!
 #
 # print() = comando de saida
-print("Alo mundo!")
+print("Ola mundo!")
+
+# Quando quiser guardar uma String! (frase)
+nome = "Joao Paulo Tenorio"
+# Quando quiser guardar um número inteiro 
+idade = 24
+# Exibir o meu nome (que está dentro da variável nome)
+print(nome)
+
 ''' 
 Comentario em bloco
 
