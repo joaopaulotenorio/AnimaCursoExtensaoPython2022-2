3ca69e817593f46c50249aabe2bea6fd129f12f6
João Paulo Tenório <44954773+joaopaulotenorio@users.noreply.github.com>
2022-10-27 01:07:36 +0000

    Aula 02 - B - Condicionais

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-# Aula 02 - A - 2022/10/26 
+# Aula 02 - B - 2022/10/26 
 
-#
+# Pede o nome do aluno e sua nota (de 0 a 10) e, se ele tirou nota 10, mostra "{nome}, você é bichão, mesmo..."
+nome = input("Informe o seu nome: ")
+nota = float(input("Digite a sua nota: "))
+
+if (nota == 10):
+  print(f"{nome}, você é bichão, mesmo...")
+elif (nota >= 6 and nota < 10):
+  print(f"{nome}, bom trabalho!")
+else:
+  print("Burro, não tirou nem seis...")
+
+
