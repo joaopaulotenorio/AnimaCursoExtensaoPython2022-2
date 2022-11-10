19d6678dcd2cccef8b7c1bc9a8d92c14658e7b66
João Paulo Tenório <44954773+joaopaulotenorio@users.noreply.github.com>
2022-11-10 01:03:59 +0000

    Funções + Listas + Laços

--- main.py
+++ main.py
@@ -17,18 +17,32 @@ print(imposto2)
 # Criar uma função calcular_imposto() que calcula um imposto de 5% e retorno à quem pediu...
 # Isso é a declaração da função (Como ele funciona)
 def calcular_imposto(preco_produto):
-  imposto = preco_produto * 0.05
+  imposto = preco_produto * 0.07
   return imposto
 
 # Aqui é o uso... Aqui é imposto a calcular..
 preco = 299
 imposto = calcular_imposto(preco)
 print(imposto)
+print(f"\nEsse aqui é com a função (7%): {imposto}")
 
 # Explicação de variável local x global
 print(preco) #???
 preco_produto = 100
 print(preco_produto) #???
 
+# Agora calcular imposto a aliquota é 7%
+valores = [1.99, 24.50, 78.27, 1515.5]
+# Se eu quiser calcular o imposto destes quatro valores... e exibir na tela assim: "O imposto de ... é ..." (1o. preço, 2o. imposto)
 
+# Forma 01
+print("\n")
+for valor in valores:
+      imposto = calcular_imposto(valor)
+      print(imposto)
+  
+print("\n")
+# Forma 02
+for valor in valores :
+  print(f"O imposto de {valor} é {calcular_imposto(valor)}")
 
